7cbea195fbefaaf715f61fbcf373b862937ba7a4
joshsj <33764106+joshsj@users.noreply.github.com>
2020-02-08 22:08:39 +0000

    Reconfig stylesheets to use path from root

--- templates/layout.php
+++ templates/layout.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./styles/main.css">
+  <link rel="stylesheet" href="/styles/main.css">
   <title><?= $title ?></title>
 </head>
 
 <body>
-  <?= $content; // template content 
+  <?= $content; // template content
   ?>
 </body>
 
