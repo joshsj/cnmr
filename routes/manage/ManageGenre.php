8481408756dcca0a7e76ed512f07182742e1d26b
joshsj <33764106+joshsj@users.noreply.github.com>
2020-02-11 03:53:01 +0000

    Implement genre updation

--- routes/manage/ManageGenre.php
+++ routes/manage/ManageGenre.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RouteHandler;
 
+use PDOException;
 use Slim\Routing\RouteCollectorProxy;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -51,7 +52,7 @@ class ManageGenre extends AbstractRouteHandler
       $q_new_genre = $db->prepare("insert into genre (name) values (?)");
       try {
         $q_new_genre->execute([$genre]);
-      } catch (\PDOException $e) {
+      } catch (PDOException $e) {
         $_SESSION["msg"] = $e->getMessage();
         return $res->withHeader("Location", "/manage/genre");
       }
@@ -59,6 +60,49 @@ class ManageGenre extends AbstractRouteHandler
       // it worked
       $_SESSION["msg"] = "'$genre' inserted successfully.";
       return $res->withHeader("Location", "/manage/genre");
+    });
+
+    $group->post("/update", function (Request $req, Response $res) use ($db) {
+      $_SESSION["msg"] = "";
+
+      // update each genre
+      foreach ($_POST as $id => $name) {
+        // counters for message
+        $deletions = 0;
+        $updates = 0;
+
+        // sanitize inputs
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+        // empty name means deletion
+        if (empty($name)) {
+          $q_delete_genre = $db->prepare("delete from genre where id = ?");
+          try {
+            $q_delete_genre->execute([$id]);
+          } catch (PDOException $e) {
+          }
+          ++$deletions;
+        } else {
+          // validate name
+          if (!ctype_alpha($name)) {
+            $_SESSION["msg"] = "Genre contains invalid characters.";
+            return $res->withHeader("Location", "/manage/genre");
+          }
+
+          // insert
+          $q_update_genre = $db->prepare("update genre set name = ? where id = ?");
+          try {
+            $q_update_genre->execute([$name, $id]);
+            // update counter with rows affected
+            $updates += $q_update_genre->rowCount();
+          } catch (PDOException $e) {
+          }
+        }
+      }
+
+      $_SESSION["msg"] = "$updates updates, $deletions deletions";
+      return $res->withHeader("Location", "/manage/genre");;
     });
   }
 }
