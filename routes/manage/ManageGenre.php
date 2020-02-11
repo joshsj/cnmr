952930d40ebf10b67a141c24a9fa735c3ed09226
joshsj <33764106+joshsj@users.noreply.github.com>
2020-02-11 03:04:09 +0000

    Implement genre adding

--- routes/manage/ManageGenre.php
+++ routes/manage/ManageGenre.php
@@ -15,13 +15,50 @@ class ManageGenre extends AbstractRouteHandler
     $db = $this->db;
 
     $root = function (Request $req, Response $res) use ($db) {
+      // get and clean session message
+      $msg = $_SESSION["msg"] ?? "";
+      $_SESSION["msg"] = "";
+
       $genres = $db->query("select * from genre order by id")->fetchAll();
 
-      $this->get("view")->render($res, "manage/genre.twig", ["genres" => $genres]);
+      $this->get("view")->render(
+        $res,
+        "manage/genre.twig",
+        ["genres" => $genres, "msg" =>  $msg]
+      );
       return $res;
     };
 
     $group->get("", $root);
     $group->get("/", $root);
+
+    // new genre
+    $group->post("/new", function (Request $req, Response $res) use ($db) {
+      // sanitize input
+      $genre = filter_input(INPUT_POST, "genre", FILTER_SANITIZE_STRING);
+
+      // validate name to just characters
+      if (!ctype_alpha($genre)) {
+        // store error message in session
+        $_SESSION["msg"] = "Genre contains invalid characters.";
+        return $res->withHeader("Location", "/manage/genre");
+      }
+
+      // format name to titlecase
+      $genre = ucwords($genre);
+
+      // insert
+      $q_new_genre = $db->prepare("insert into genre (name) values (?)");
+      try {
+        $q_new_genre->execute([$genre]);
+      } catch (\PDOException $e) {
+        $_SESSION["msg"] = $e->getMessage();
+        return $res->withHeader("Location", "/manage/genre");
+      }
+
+      // it worked
+      $_SESSION["msg"] = "'$genre' inserted successfully.";
+      return $res->withHeader("Location", "/manage/genre");
+    });
   }
 }
