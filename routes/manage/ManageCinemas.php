00de45a3a191e3ca7cf6e160ead34652c6da89ce
joshsj <33764106+joshsj@users.noreply.github.com>
2020-02-11 22:13:28 +0000

    Implement cinema update

--- routes/manage/ManageCinemas.php
+++ routes/manage/ManageCinemas.php
@@ -24,12 +24,72 @@ class ManageCinemas extends AbstractRouteHandler
     $group->get("/", $root);
 
     $group->get("/{id}", function (Request $req, Response $res, array $args) use ($db) {
+      $msg = $_SESSION["msg"] ?? "";
+      $_SESSION["msg"] = "";
+
       $q_cinema = $db->prepare("select * from cinema where id = ?");
       $q_cinema->execute([$args["id"]]);
       $cinema = $q_cinema->fetch();
 
-      $this->get("view")->render($res, "/manage/cinemas-id.twig", ["cinema" => $cinema]);
+      $this->get("view")->render(
+        $res,
+        "/manage/cinemas-id.twig",
+        [
+          "cinema" => $cinema, "msg" => $msg
+        ]
+      );
       return $res;
+    });
+
+    // update
+    $group->post("/{id}/update", function (Request $req, Response $res, array $args) use ($db) {
+      $id = $args["id"];
+
+      $updates = 0;
+
+      // update fields with counter
+      $shop = isset($_POST["shop"]) ? true : false;
+      $stmt = $db->prepare("update cinema set shop = ? where id = ?");
+      $stmt->execute([$shop, $id]);
+      $updates += $stmt->rowCount();
+
+      $validate = function ($key) {
+        $s = filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING);
+        if (preg_match('/^[a-zA-Z0-9 ]*$/', $s)) {
+          return ucwords($s);
+        } else {
+          return false;
+        }
+      };
+
+      if ($address = $validate("address")) {
+        $stmt = $db->prepare("update cinema set address = ? where id = ?");
+        $stmt->execute([$address, $id]);
+        $updates += $stmt->rowCount();
+      }
+
+      if ($area = $validate("area")) {
+        $stmt = $db->prepare("update cinema set area = ? where id = ?");
+        $stmt->execute([$area, $id]);
+        $updates += $stmt->rowCount();
+      }
+
+      if ($city = $validate("city")) {
+        $stmt = $db->prepare("update cinema set city = ? where id = ?");
+        $stmt->execute([$city, $id]);
+        $updates += $stmt->rowCount();
+      }
+
+      if ($pc = $validate("postcode")) {
+        $stmt = $db->prepare("update cinema set postcode = ? where id = ?");
+        $stmt->execute([$pc, $id]);
+        $updates += $stmt->rowCount();
+      }
+
+      // updates session message
+      $_SESSION["msg"] = "$updates updates";
+
+      return $res->withHeader("Location", "/manage/cinemas/$id");
     });
   }
 }
