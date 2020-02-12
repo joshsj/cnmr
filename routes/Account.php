68fb28073ec1ec0145c381e44534b6083baec01f
joshsj <33764106+joshsj@users.noreply.github.com>
2020-02-12 19:56:42 +0000

    Implement account tickets

--- routes/Account.php
+++ routes/Account.php
@@ -12,14 +12,43 @@ class Account extends AbstractRouteHandler
 {
   public function __invoke(RouteCollectorProxy $group)
   {
-    $group->get("", function (Request $req, Response $res, array $args) {
+    $db = $this->db;
+
+    $group->get("", function (Request $req, Response $res, array $args) use ($db) {
       // check logged in
-      if (isset($_SESSION["email"])) {
-        // show account page
-        $this->get("view")->render($res, "account.twig", ["account" => $_SESSION]);
-      } else {
-        $res = $res->withHeader("Location", "/login"); // redirect to login page
+      if (!isset($_SESSION["email"])) {
+        return $res->withHeader("Location", "/login"); // redirect to login page
       }
+
+      // get account tickets
+      $stmt = $db->prepare(
+        'select screening.start, film.title as "film",
+        cinema.area as "cinema_area", cinema.city as "cinema_city"
+        from booking
+        inner join screening on fk_screening = screening.id
+        -- get film name
+        inner join film on screening.fk_film = film.id
+        -- get cinema info
+        inner join screen on screening.fk_screen = screen.id
+        inner join cinema on screen.fk_cinema = cinema.id
+        -- get account id
+        inner join account on booking.fk_account = account.id
+        where account.email = ?'
+      );
+      $stmt->execute([$_SESSION["email"]]);
+      $bookings = $stmt->fetchAll();
+
+      $this->get("view")->render(
+        $res,
+        "account.twig",
+        [
+          "account" => [
+            "email" => $_SESSION["email"],
+            "admin" => $_SESSION["admin"]
+          ],
+          "bookings" => $bookings
+        ]
+      );
       return $res;
     });
   }
